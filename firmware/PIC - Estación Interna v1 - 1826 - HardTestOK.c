41f9ccb4958b7b486e68ad81d835babaf102539f
Electgpl <electgpl@gmail.com>
2017-10-12 20:57:35 -0300

    Rename PIC - Estación Interna v1 - 1826 - HardTestOK.c to firmware/PIC - Estación Interna v1 - 1826 - HardTestOK.c

--- firmware/PIC - Estación Interna v1 - 1826 - HardTestOK.c
+++ firmware/PIC - Estación Interna v1 - 1826 - HardTestOK.c
@@ -1,14 +1,14 @@
 //*********************************************************************************************************
-// Programa para la estaci髇 meteorol骻ica Interna inal醡brica
-// El programa recibe Temperatura, Humedad y Luminosidad por interrupci髇
+// Programa para la estaci贸n meteorol贸gica Interna inal谩mbrica
+// El programa recibe Temperatura, Humedad y Luminosidad por interrupci贸n
 // El sistema mide Temperatura y Humedad interior
-// El enlace se realiza mediante una comunicaci髇 ASK OOK UHF sobre UART Invertido a 600bps
-// Se muestra medici髇 interna y externa en un LCD de 2x16
+// El enlace se realiza mediante una comunicaci贸n ASK OOK UHF sobre UART Invertido a 600bps
+// Se muestra medici贸n interna y externa en un LCD de 2x16
 //*********************************************************************************************************
 #include <16F1826.h>                                           //Biblioteca del microcontrolador
-#fuses NOMCLR, NOWDT, INTRC_IO                                 //Configuraci髇 de fuses
-#use delay(int=4000000)                                        //Configuraci髇 del clock interno a 4MHz
-#use rs232(baud=600,parity=N,xmit=PIN_B2,rcv=PIN_B1,bits=8)    //Configuraci髇 de UART 600bps 8 N 1
+#fuses NOMCLR, NOWDT, INTRC_IO                                 //Configuraci贸n de fuses
+#use delay(int=4000000)                                        //Configuraci贸n del clock interno a 4MHz
+#use rs232(baud=600,parity=N,xmit=PIN_B2,rcv=PIN_B1,bits=8)    //Configuraci贸n de UART 600bps 8 N 1
 #define LCD_ENABLE_PIN  PIN_A7                                 //Redefine pin de LCD
 #define LCD_RS_PIN      PIN_A4                                 //Redefine pin de LCD
 #define LCD_RW_PIN      PIN_A6                                 //Redefine pin de LCD
@@ -20,23 +20,23 @@
 //*********************************************************************************************************
 // Variables
 //*********************************************************************************************************
-#define HEADER 200                                             //Definici髇 de valor Header para el payload
+#define HEADER 200                                             //Definici贸n de valor Header para el payload
 #define DATA PIN_A3                                            //Pin del bus de un hilo para el DHT11
 #define STATUS PIN_A1                                          //Pin para analizar el status de la interrupt
 static int8 datoInt[2];                                        //Variable donde se alojan los datos internos
 static int8 payload[5];                                        //Variable donde se aloja el payload
 static int8 datoExt[3];                                        //Variable donde se alojan los datos externos
 //*********************************************************************************************************
-// Funci髇 Interrupci髇 UART que realiza el parse de datos, validaci髇 de Header y Checksum
-// Si validaci髇 y Checksum son validos, carga el vector DATO para ser utilizado
+// Funci贸n Interrupci贸n UART que realiza el parse de datos, validaci贸n de Header y Checksum
+// Si validaci贸n y Checksum son validos, carga el vector DATO para ser utilizado
 //*********************************************************************************************************
 int8 i=0;                                                      //Variable para el contador de bytes de entrada
-#int_RDA                                                       //Interrupci髇 por dato en UART
-void intser(){                                                 //Funci髇 de servicio de interrupci髇
+#int_RDA                                                       //Interrupci贸n por dato en UART
+void intser(){                                                 //Funci贸n de servicio de interrupci贸n
    payload[i++]=getc();                                        //Guarda byte de entrada en payload. incrementa indice
    if(i>5){                                                    //Si el indice es mayor que 5 se asume que se completa el payload
-      if(payload[0]==HEADER){                                  //Validaci髇 que el Header sea 200 (seteado en el transmisor)
-         if(payload[1]+payload[2]+payload[3]-payload[4]==0){   //Validaci髇 de checksum, si datos le韉os son igual a checksum
+      if(payload[0]==HEADER){                                  //Validaci贸n que el Header sea 200 (seteado en el transmisor)
+         if(payload[1]+payload[2]+payload[3]-payload[4]==0){   //Validaci贸n de checksum, si datos le铆dos son igual a checksum
             datoExt[0]=payload[1];                             //Cargamos los datos en el vector
             datoExt[1]=payload[2];                             //Cargamos los datos en el vector
             datoExt[2]=payload[3];                             //Cargamos los datos en el vector
@@ -62,14 +62,14 @@ void intser(){                                                 //Funci髇 de serv
 //*********************************************************************************************************
 unsigned int trama[5];                                         //Vector donde se alojan los datos
 //*********************************************************************************************************
-// Funci髇 de recepci髇 de Byte
-// Lee el valor le韉o en la trama y lo separa realizando shift
-// Retorna el valor en forma de byte, es utilizado en la funci髇 recibeDato()
+// Funci贸n de recepci贸n de Byte
+// Lee el valor le铆do en la trama y lo separa realizando shift
+// Retorna el valor en forma de byte, es utilizado en la funci贸n recibeDato()
 //*********************************************************************************************************
-unsigned int recibeByte(){                                     //Funci髇 que recibe un Byte
-   unsigned int8 valorLeido = 0;                               //Valor de retorno de la funci髇
-   int8 i=0;                                                   //Inicializaci髇 del indice
-   for(i=0; i<8; i++){                                         //Iteraci髇 para recepci髇 de bits
+unsigned int recibeByte(){                                     //Funci贸n que recibe un Byte
+   unsigned int8 valorLeido = 0;                               //Valor de retorno de la funci贸n
+   int8 i=0;                                                   //Inicializaci贸n del indice
+   for(i=0; i<8; i++){                                         //Iteraci贸n para recepci贸n de bits
       valorLeido <<= 1;                                        //Registro de desplazamiento de bits
       while(!input(DATA));                                     //Espera a DATA = 0
       delay_us(30);                                            //Demora de 30us (Del Datasheet)
@@ -81,12 +81,12 @@ unsigned int recibeByte(){                                     //Funci髇 que rec
    return valorLeido;                                          //Retorna el valor leido
 }
 //*********************************************************************************************************
-// Funci髇 de recepci髇 de dato para el DHT11
+// Funci贸n de recepci贸n de dato para el DHT11
 // Recibe los valores de temperatura y humedad (parte entera y decimales por separado)
-// Recibe el checksum enviado por el DHT11 y lo compara con el le韉o en el programa
+// Recibe el checksum enviado por el DHT11 y lo compara con el le铆do en el programa
 //*********************************************************************************************************
-unsigned int recibeDato(){                                     //Funci髇 que recibe el Dato
-   int validacion = 0;                                         //Variable de Validaci髇
+unsigned int recibeDato(){                                     //Funci贸n que recibe el Dato
+   int validacion = 0;                                         //Variable de Validaci贸n
    int checksum = 0;                                           //Variable de deteccion de cambios de secuencia
    int8 j=0;                                                   //Variable para el lazo for
    output_high(DATA);                                          //Set DATA = 1  
@@ -97,7 +97,7 @@ unsigned int recibeDato(){                                     //Funci髇 que rec
    validacion = input(DATA);                                   //Mueve valor de DATA a Validacion
    delay_us(80);                                               //Espera 80us (Del Datasheet)
    validacion = input(DATA);                                   //Mueve valor de DATA a Validacion
-   if(!validacion){                                            //Si Validaci髇 = 0, Error de secuencia
+   if(!validacion){                                            //Si Validaci贸n = 0, Error de secuencia
       printf( "Error en Checksum \r");                         //Muestra leyenda de error
    }
    delay_us(80);                                               //Espera 80us (Del Datasheet)
@@ -105,17 +105,17 @@ unsigned int recibeDato(){                                     //Funci髇 que rec
        trama[j] = recibeByte();                                //Carga del vector de datos
    }
    output_high(DATA);                                          //Set DATA = 1
-   for(j=0; j<4; j++){                                         //Lazo de carga de bytes de verificaci髇
-       checksum += trama[j];                                   //Carga de bytes de verificaci髇
+   for(j=0; j<4; j++){                                         //Lazo de carga de bytes de verificaci贸n
+       checksum += trama[j];                                   //Carga de bytes de verificaci贸n
    }
-   if(checksum == trama[4]){                                   //Si la secuencia de verificaci髇 es correcta
+   if(checksum == trama[4]){                                   //Si la secuencia de verificaci贸n es correcta
       return 0;                                                //Se retorna 0 y se realiza la lectura
    }
 }
 //*********************************************************************************************************
 // Lee DHT11
 //*********************************************************************************************************
-void leeDHT11(void){                                           //Funci髇 que lee el DHT11
+void leeDHT11(void){                                           //Funci贸n que lee el DHT11
    if(recibeDato()==0){                                        //Consulta si hay dato en la entrada del DHT11
       datoInt[0]=trama[2];                                     //Carga el valor de temperatura DHT en byte 1 del payload
       datoInt[1]=trama[0];                                     //Carga el valor de humedad DHT en byte 2 del payload
@@ -124,18 +124,18 @@ void leeDHT11(void){                                           //Funci髇 que lee
 //*********************************************************************************************************
 // Saludo de Pantalla
 //*********************************************************************************************************
-void saludoPantalla(void){                                     //Funci髇 que realiza un saludo inicial
+void saludoPantalla(void){                                     //Funci贸n que realiza un saludo inicial
    lcd_gotoxy(1,1);                                            //Posiciona el cursor en la pantalla
-   printf(lcd_putc," Est.Inalambrica");                        //Imprime mensaje rengl髇 1
+   printf(lcd_putc," Est.Inalambrica");                        //Imprime mensaje rengl贸n 1
    lcd_gotoxy(1,2);                                            //Posiciona el cursor en la pantalla
-   printf(lcd_putc," v1.1  UTN INSPT");                        //Imprime mensaje rengl髇 2
+   printf(lcd_putc," v1.1  UTN INSPT");                        //Imprime mensaje rengl贸n 2
    delay_ms(2000);                                             //Delay de saludo
    lcd_init();                                                 //Inicializa LCD
 }
 //*********************************************************************************************************
 // Impresion de Pantalla
 //*********************************************************************************************************
-void impPantalla(void){                                        //Funci髇 que imprime pantalla
+void impPantalla(void){                                        //Funci贸n que imprime pantalla
    lcd_gotoxy(1,2);                                            //Posiciona el cursor en la pantalla
    printf(lcd_putc,"%2u",datoExt[0]);                          //Imprime Temperatura Exterior
    lcd_gotoxy(3,2);                                            //Posiciona el cursor en la pantalla
@@ -162,17 +162,17 @@ void impPantalla(void){                                        //Funci髇 que imp
    lcd_putc("%RH");                                            //Imprime unidad de medida
 }
 //*********************************************************************************************************
-// Programa principal, Realiza la lectura de DHT11, lectura de bater韆 y recibe los datos por UART
-// Lee la temperatura y humedad interna, realiza un pron髎tico aproximado
+// Programa principal, Realiza la lectura de DHT11, lectura de bater铆a y recibe los datos por UART
+// Lee la temperatura y humedad interna, realiza un pron贸stico aproximado
 //*********************************************************************************************************
-void main(){                                                   //Funci髇 principal
+void main(){                                                   //Funci贸n principal
    enable_interrupts(global);                                  //Habilita interrupciones globales
-   enable_interrupts(int_rda);                                 //Habilita interrupci髇 de dato en UART
+   enable_interrupts(int_rda);                                 //Habilita interrupci贸n de dato en UART
    lcd_init();                                                 //Inicializa LCD
-   saludoPantalla();                                           //Funci髇 de saludo de pantalla
+   saludoPantalla();                                           //Funci贸n de saludo de pantalla
    while(true){                                                //Loop principal infinito
-      leeDHT11();                                              //Funci髇 que lee DHT11
-      delay_ms(2000);                                          //Delay de Actualizaci髇
+      leeDHT11();                                              //Funci贸n que lee DHT11
+      delay_ms(2000);                                          //Delay de Actualizaci贸n
       impPantalla();                                           //Imprime pantalla LCD
       delay_ms(2000);                                          //Delay de Actualizacion
    }
